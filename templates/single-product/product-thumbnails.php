bdefaef364ed8695617c0b5724e6fc92bdc06dcb
Nizo <martin.niznansky@gmail.com>
2018-06-30 21:19:40 +0200

    gallery - limit max images on single-product page

--- templates/single-product/product-thumbnails.php
+++ templates/single-product/product-thumbnails.php
@@ -16,25 +16,26 @@ $attachment_ids = $product->get_gallery_attachment_ids();
 
 if ( $attachment_ids ) {
 	?>
-	<div class="thumbnails"><?php
+	<div class="thumbnails">
+		<?php
 
 		$loop = 0;
 		$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
 
 		foreach ( $attachment_ids as $attachment_id ) {
 
-			$classes = array( 'zoom' );
+			$classes = array( 'zoom' );			
 			
-			if ($loop > 2)
+			if ($loop > 5) {
 				$classes[] = 'hidden';
-				
-			/*
+			}
+
 			if ( $loop == 0 || $loop % $columns == 0 )
 				$classes[] = 'first';
 
 			if ( ( $loop + 1 ) % $columns == 0 )
 				$classes[] = 'last';
-			*/
+			
 
 			$image_link = wp_get_attachment_url( $attachment_id );
 
@@ -45,7 +46,23 @@ if ( $attachment_ids ) {
 			$image_class = esc_attr( implode( ' ', $classes ) );
 			$image_title = esc_attr( get_the_title( $attachment_id ) );
 
-			echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<a href="%s" class="%s" title="%s" data-rel="prettyPhoto[product-gallery]">%s</a>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
+			//echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', sprintf( '<a href="%s" class="%s" title="%s" data-rel="prettyPhoto[product-gallery]">%s</a>', $image_link, $image_class, $image_title, $image ), $attachment_id, $post->ID, $image_class );
+
+			//woocommerce_single_product_image_thumbnail_html START
+			$image_link = wp_get_attachment_url( $attachment_id );			
+			$image = '<div style="background-image: url(\'' . $image_link. '\');"></div>';
+			$image_title = esc_attr(get_post_field('post_content', $attachment_id));
+
+
+			if ($loop == 5) {
+				//$image = esc_attr('<div>+ ' .  . '</div>');				
+
+				$text = '+' . (count($attachment_ids) - $loop);
+				$image = '<div class="remaining-gallery-images-count">' . $text . '</div>';
+			}
+
+			$img = sprintf( '<a href="%s" class="%s" title="%s"  rel="prettyPhoto[product-gallery]">%s</a>', $image_link, $image_class, $image_title, $image );
+			echo $img;
 
 			$loop++;
 		}
