64dce966ce9c0afcb090f2d8597c775f02aaa6eb
Martin Niznansky <nizo@Abyss.domain>
2018-11-08 11:02:06 +0100

    Order value in cart ( display without shipping costs.. )

--- templates/cart/cart-totals-netto.php
+++ templates/cart/cart-totals-netto.php
@@ -9,29 +9,35 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
-<div>	
+<div>
 
 	<div class="price-subtotal-section">
-		<div class="price-title"><?php _e( 'Cart Value Totals', 'woocommerce' ); ?></div>	
+		<div class="price-title"><?php _e( 'Cart Value Totals', 'woocommerce' ); ?></div>
 		<div class="price-subtotal">
-			
-			
-			<span class="amount"><?php echo WC()->cart->get_total(); ?></span> <?php echo WC()->countries->inc_tax_or_vat(); ?>
+
+			<span class="amount">
+				<?php
+					wc_cart_totals_subtotal_html();
+					//$cartTotal = WC()->cart->get_total();
+					//echo $cartTotal;
+				?>
+			</span>
+
+			<?php echo WC()->countries->inc_tax_or_vat(); ?>
 			<?php
 				/*
-				$discount = WC()->cart->get_total_discount(); 
+				$discount = WC()->cart->get_total_discount();
 				if ($discount != false) {
 					echo '<div class="cart-discount-value">' . __('So zľavou:', 'woocommerce') . ' ' . $discount . '</div>';
-				}				
+				}
 				*/
 			?>
 			<!--
-				wc_cart_totals_subtotal_html();
 				echo ' ' . WC()->countries->inc_tax_or_vat();
 			-->
 		</div>
-	</div>				
+	</div>
 	<small class="cart-shipping-in-next-step">
 	 <?php _e( 'Note: Shipping and taxes are estimated%s and will be updated during checkout based on your billing and shipping information.', 'woocommerce' ); ?>
-	</small>				
+	</small>
 </div>
