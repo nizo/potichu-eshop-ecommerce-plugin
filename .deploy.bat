9a9e5602ed2492a966faa886e8878a1180629b69
nizo <martin.niznansky@gmail.com>
2019-02-09 21:01:16 +0100

    deployment script fix

--- .deploy.bat
+++ .deploy.bat
@@ -6,14 +6,12 @@ title Potichu Woocommerce Plugin - Deployment Tool
 :Menu
 echo.
 echo.
-echo Select Task
-echo ===========
+echo Eshop Woocommerce
+echo =================
 echo.
 echo 0. PHPloy status
-echo 1. Git status
-echo 2. Build new version
-echo 3. Push new version to Git
-echo 4. Deploy to server
+echo 1. Build new version
+echo 2. Deploy to server
 echo 9. Exit
 
 echo.
@@ -22,25 +20,12 @@ set /p a=
 if "%a%"=="0" (
 	GOTO PhployStatus
 ) 
-if "%a%"=="1" (
-	echo.
-	echo.
-	echo Git status
-	echo ==========
-	echo.
-	call git status
-	GOTO Menu
-) 
 
-if "%a%"=="2" (
+if "%a%"=="1" (
 	GOTO BuildNewVersion
 )
 
-if "%a%"=="3" (	
-	GOTO PushToGit
-) 
-
-if "%a%"=="4" (
+if "%a%"=="2" (
 	GOTO Deploy
 )
 
@@ -55,20 +40,6 @@ echo Build new version
 echo =================
 echo.
 call gulp
-GOTO Menu
-
-
-:PushToGit
-
-echo.
-echo.
-echo Enter commit description
-echo ========================
-echo.
-set /p d=
-call git add -A
-call git commit -m "%d%" -a
-call git push
 GOTO Menu
 
 
